6dd9f9d4a36604a0eb338166ca70b859e2a6b17d
Tom Taylor <theetommyt@gmail.com>
2015-11-30 09:42:48 -0600

    slider height

--- cumuloTheme/cumulo/index.php
+++ cumuloTheme/cumulo/index.php
@@ -4,10 +4,6 @@ $cmo_list_style = cmo_get_theme_mod_value( "blog-list-style" );
 $cmo_sidebar = cmo_get_page_sidebar();
 ?>
 
-<!-- slider?  -->
-<?php putRevSlider('slider1', 'homepage'); ?>
-
-
 <div id="main-container">
 	<div class="page-content container page-index blog-list-style-<?php echo esc_attr( $cmo_list_style ) ?> <?php echo esc_attr( $cmo_sidebar[1] ); ?>">
 		<?php get_template_part( "templates/" . esc_attr( $cmo_list_style ) . "/layout" ); ?>
